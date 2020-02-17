80f9eb1c483f3516c5e9e5e7da52639094ba0650
casper-hansen <repsac2002@gmail.com>
2020-02-17 22:25:59 +0100

    com idk...
    Skrev lite fint java script och fortsatte stylea till min mobilsida

--- js/navAnimate.js
+++ js/navAnimate.js
@@ -1,1 +1,34 @@
-var scroll = window.requestAnimationFrame
+function isElementInViewport(el) {
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+      el = el[0];
+    }
+    var rect = el.getBoundingClientRect();
+    return (
+      (rect.top <= 0
+        && rect.bottom >= 0)
+      ||
+      (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+      ||
+      (rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    );
+  }
+
+var scroll = window.requestAnimationFrame ||
+    function(callback) {window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll(".onscroll");
+function loop() {
+    elementsToShow.forEach(function (element) { 
+        if (isElementInViewport(element)) {
+            element.classList.add("animation")
+        }
+        else {
+            element.classList.remove("animation")
+        }
+    });
+    scroll(loop);
+}
+
+loop();
+
